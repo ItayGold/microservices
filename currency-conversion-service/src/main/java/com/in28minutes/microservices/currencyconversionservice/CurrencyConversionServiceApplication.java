fbf4ebcb3ac370e1b5e11b289bc5ecdd360a944a
ItayGold <itil.gold@gmail.com>
2020-11-30 11:53:33 +0200

    work release

--- currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,10 +2,9 @@ package com.in28minutes.microservices.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients("com.in28minutes.microservices.currencyconversionservice")
+//@EnableFeignClients("com.in28minutes.microservices.currencyconversionservice")
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
